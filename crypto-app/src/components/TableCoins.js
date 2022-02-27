cf110377075e7ce62631d9940afab041cb81d604
FacuRearte <facundorearte13@gmail.com>
2022-02-27 10:13:29 -0300

    a

--- crypto-app/src/components/TableCoins.js
+++ crypto-app/src/components/TableCoins.js
@@ -4,7 +4,9 @@ import { CoinRow } from "./CoinRow";
 const titles = ["Index", "Coin", "Price", "Price Change", "24hr volume"];
 
 export const TableCoins = ({ coins, search }) => {
-    const filteredCoins = coins.filter((coin) => coin.name.toLowerCase().includes(search.toLowerCase()))
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
   return (
     <table className="table table-dark mt-4 table-hover">
       <thead>
@@ -15,9 +17,13 @@ export const TableCoins = ({ coins, search }) => {
         </tr>
       </thead>
       <tbody>
-        {filteredCoins ? filteredCoins.map((coin, index) => (<CoinRow coin={coin} key={index} index={index} />)) : coins.map((coin, index) => (
-          <CoinRow coin={coin} key={index} index={index} />
-        ))}
+        {filteredCoins
+          ? filteredCoins.map((coin, index) => (
+              <CoinRow coin={coin} key={index} index={index} />
+            ))
+          : coins.map((coin, index) => (
+              <CoinRow coin={coin} key={index} index={index} />
+            ))} 
       </tbody>
     </table>
   );
